cbd418f98378903249eb87a5eb4543be1d800431
Victoria Blackburn <victoriablackburn@Victorias-Air.lan>
2020-11-15 19:40:33 -0800

    uploading to github

--- employee/src/index.js
+++ employee/src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './App.js';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
 // import 'bootstrap/dist/css/bootstrap.min.css';
 
 
-ReactDOM.render(<App/>, doocument.getElementById ('root'));
+ReactDOM.render(<App/>, document.getElementById ('root'));
 
 serviceWorker.unregister();
