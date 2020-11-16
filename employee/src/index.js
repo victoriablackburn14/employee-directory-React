0d64634b87a3029218eacdc5bba0e376eb521819
Victoria Blackburn <victoriablackburn@Victorias-Air.lan>
2020-11-15 19:19:29 -0800

    updated data area

---
+++ employee/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import * as serviceWorker from './serviceWorker';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+ReactDOM.render(<App/>, doocument.getElementById ('root'));
+
+serviceWorker.unregister();
